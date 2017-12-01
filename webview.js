3b20f1a03825fa9f849cb2b83d4b86ca732e5fef
Stefan Malzner <stefan@adlk.io>
2017-12-01 19:51:22 +0100

    filter muted chats

--- webview.js
+++ webview.js
@@ -5,7 +5,7 @@ module.exports = (Franz) => {
     let count = 0;
     const searchElement = document.querySelector('.im_dialogs_search_field');
     if (searchElement && searchElement.value === '') {
-      const elements = document.querySelectorAll('.im_dialog_badge:not(.ng-hide)');
+      const elements = document.querySelectorAll('.im_dialog_badge:not(.ng-hide):not(.im_dialog_badge_muted)');
       if (elements) {
         for (let i = 0; i < elements.length; i += 1) {
           if (elements[i].innerHTML !== 0) {
