a789a49bb4ad4d0e403f44198b0a2aac870d8cba
Stefan Malzner <stefan@adlk.io>
2021-11-03 14:03:11 +0100

    fix message count & page width

--- webview.js
+++ webview.js
@@ -3,14 +3,12 @@ const path = require('path');
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
     let count = 0;
-    const searchElement = document.querySelector('.im_dialogs_search_field');
-    if (searchElement && searchElement.value === '') {
-      const elements = document.querySelectorAll('.im_dialog_badge:not(.ng-hide):not(.im_dialog_badge_muted)');
-      if (elements) {
-        for (let i = 0; i < elements.length; i += 1) {
-          if (elements[i].innerHTML !== 0) {
-            count += 1;
-          }
+    const elements = document.querySelectorAll('.chatlist .rp:not(.is-muted) .unread');
+
+    if (elements) {
+      for (let i = 0; i < elements.length; i += 1) {
+        if (elements[i].innerHTML !== 0) {
+          count += 1;
         }
       }
     }
