ea622a5a762b3106aaf9b2c3e15f2b0889f5ff5e
Stefan Malzner <stefan@adlk.io>
2022-04-04 13:19:26 +0200

    Support Z & K version

--- webview.js
+++ webview.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
     let count = 0;
-    const elements = document.querySelectorAll('.chatlist .rp:not(.is-muted) .unread');
+    const elements = document.querySelectorAll('.chatlist .rp:not(.is-muted) .unread, .Badge.unread:not(.muted)');
 
     if (elements) {
       for (let i = 0; i < elements.length; i += 1) {
